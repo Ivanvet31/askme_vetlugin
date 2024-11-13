f838127fcf1ba486fcbe7e18ce7c0765fc6a513d
ivanvet31 <ivanvet31@gmail.com>
2024-11-13 21:10:26 +0300

    бутстрап умер

--- app/logic/pagination.py
+++ app/logic/pagination.py
@@ -1,25 +1,23 @@
-'''
-очень сырое, нужно дожаривать
-'''
-
 def paginations_params(page_num, paginator, page):
     two_pages_ahead = page_num + 2
     two_pages_behind = page_num - 2
+
     try:
         arrows_left_border = paginator.page(two_pages_behind)
         left_switch = True
-    except:
+    except (IndexError, ValueError):
         left_switch = False
         arrows_left_border = None
+
     try:
         arrows_right_border = paginator.page(two_pages_ahead)
         right_switch = True
-    except:
+    except (IndexError, ValueError):
         right_switch = False
         arrows_right_border = None
 
     is_first_page = True if page.number == 1 else False
-    is_last_page = True if page.number == paginator.num_pages else False
+    is_last_page = True if page.number == len(paginator.page_range) else False
 
     params = {
         'left_switch': left_switch,
